7d00cab25fa5b1bd9e8eeae13812cf1f8afcc34f
botato <botatostuff@gmail.com>
2021-12-28 18:44:08 -0500

    jsdoc

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 /* jshint esversion: 8 */
 
+/**
+ * Filters the URL to remove the `cc.loginfra.com` domain.
+ * @param {string} url
+ * @returns {(string|null)} The filtered URL or `null` if the URL is not valid.
+ */
 function filter_url(url) {
   url = new URL(url);
 
@@ -12,6 +17,12 @@ function filter_url(url) {
   }
 }
 
+/**
+ * Handles requests that go to `cc.loginfra.com`.
+ * @param {object} requestDetails
+ * @param {string} requestDetails.url
+ * @returns {object} { redirectUrl: string }
+ */
 function handleBeforeRequest(requestDetails) {
   if (!requestDetails.url) return;
 
